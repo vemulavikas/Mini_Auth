aab13369104ac38811de04db5aba0404b560b58a
Vemula_Vikas <126441973+vemulavikas@users.noreply.github.com>
2025-11-01 10:31:07 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  refreshTokens: { type: [String], default: [] }, // persist refresh tokens per user
+  refreshTokens: [{ type: String }], // 👈 store refresh tokens here
 });
 
 export default mongoose.model("User", userSchema);
